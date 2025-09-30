667e829cb40036f478a3d57522ba8d05095a29ce
root <root@ip-10-42-67-167.ec2.internal>
2025-09-30 12:53:10 +0000

    fix: I corrected the code in the _printf.c file

--- _printf.c
+++ _printf.c
@@ -9,8 +9,7 @@ int _printf(const char *format, ...)
 {
     va_list args;
     int i = 0, count = 0;
-    char c;
-    char *s;
+    char c, *s;
 
     if (format == NULL)
         return (-1);
@@ -22,10 +21,17 @@ int _printf(const char *format, ...)
         if (format[i] == '%')
         {
             i++;
+            /* if % is last, return error */
+            if (format[i] == '\0')
+            {
+                va_end(args);
+                return (-1);
+            }
+
             if (format[i] == 'c')
             {
                 c = (char)va_arg(args, int);
-                write(1, &c, 1);
+                if (write(1, &c, 1) != 1) { va_end(args); return (-1); }
                 count++;
             }
             else if (format[i] == 's')
@@ -35,27 +41,27 @@ int _printf(const char *format, ...)
                     s = "(null)";
                 while (*s)
                 {
-                    write(1, s, 1);
+                    if (write(1, s, 1) != 1) { va_end(args); return (-1); }
                     count++;
                     s++;
                 }
             }
             else if (format[i] == '%')
             {
-                write(1, "%", 1);
+                if (write(1, "%", 1) != 1) { va_end(args); return (-1); }
                 count++;
             }
             else
             {
-                /* If anything is unknown, type a letter. */
-                write(1, "%", 1);
-                write(1, &format[i], 1);
+                /* unknown specifier, just print it */
+                if (write(1, "%", 1) != 1) { va_end(args); return (-1); }
+                if (write(1, &format[i], 1) != 1) { va_end(args); return (-1); }
                 count += 2;
             }
         }
         else
         {
-            write(1, &format[i], 1);
+            if (write(1, &format[i], 1) != 1) { va_end(args); return (-1); }
             count++;
         }
         i++;
