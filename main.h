2dd5a75946d1d4910838253153135bc4803956d7
root <root@ip-10-42-67-167.ec2.internal>
2025-09-30 12:57:13 +0000

    fix: Betty style the filr main.h

--- main.h
+++ main.h
@@ -2,7 +2,11 @@
 #define MAIN_H
 #include <stdarg.h>
 #include <unistd.h>
-
+/**
+ * _printf - simplified printf function
+ * @format: format string
+ * Return: number of chars printed or -1 on error
+ */
 int _printf(const char *format, ...);
 
 #endif
