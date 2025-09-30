07e7afee3c6ff707c72df64f2eec1aba2fa7fa94
root <root@ip-10-42-67-167.ec2.internal>
2025-09-30 12:55:27 +0000

    fix: Betty style

--- _printf.c
+++ _printf.c
@@ -7,66 +7,90 @@
  */
 int _printf(const char *format, ...)
 {
-    va_list args;
-    int i = 0, count = 0;
-    char c, *s;
+	va_list args;
+	int i = 0, count = 0;
+	char c, *s;
 
-    if (format == NULL)
-        return (-1);
+	if (format == NULL)
+		return (-1);
 
-    va_start(args, format);
+	va_start(args, format);
 
-    while (format[i] != '\0')
-    {
-        if (format[i] == '%')
-        {
-            i++;
-            /* if % is last, return error */
-            if (format[i] == '\0')
-            {
-                va_end(args);
-                return (-1);
-            }
+	while (format[i] != '\0')
+	{
+		if (format[i] == '%')
+		{
+			i++;
+			/* if % is last, return error */
+			if (format[i] == '\0')
+			{
+				va_end(args);
+				return (-1);
+			}
 
-            if (format[i] == 'c')
-            {
-                c = (char)va_arg(args, int);
-                if (write(1, &c, 1) != 1) { va_end(args); return (-1); }
-                count++;
-            }
-            else if (format[i] == 's')
-            {
-                s = va_arg(args, char *);
-                if (s == NULL)
-                    s = "(null)";
-                while (*s)
-                {
-                    if (write(1, s, 1) != 1) { va_end(args); return (-1); }
-                    count++;
-                    s++;
-                }
-            }
-            else if (format[i] == '%')
-            {
-                if (write(1, "%", 1) != 1) { va_end(args); return (-1); }
-                count++;
-            }
-            else
-            {
-                /* unknown specifier, just print it */
-                if (write(1, "%", 1) != 1) { va_end(args); return (-1); }
-                if (write(1, &format[i], 1) != 1) { va_end(args); return (-1); }
-                count += 2;
-            }
-        }
-        else
-        {
-            if (write(1, &format[i], 1) != 1) { va_end(args); return (-1); }
-            count++;
-        }
-        i++;
-    }
+			if (format[i] == 'c')
+			{
+				c = (char)va_arg(args, int);
+				if (write(1, &c, 1) != 1)
+				{
+					va_end(args);
+					return (-1);
+				}
+				count++;
+			}
+			else if (format[i] == 's')
+			{
+				s = va_arg(args, char *);
+				if (s == NULL)
+					s = "(null)";
+				while (*s)
+				{
+					if (write(1, s, 1) != 1)
+					{
+						va_end(args);
+						return (-1);
+					}
+					count++;
+					s++;
+				}
+			}
+			else if (format[i] == '%')
+			{
+				if (write(1, "%", 1) != 1)
+				{
+					va_end(args);
+					return (-1);
+				}
+				count++;
+			}
+			else
+			{
+				/* unknown specifier, just print it */
+				if (write(1, "%", 1) != 1)
+				{
+					va_end(args);
+					return (-1);
+				}
+				if (write(1, &format[i], 1) != 1)
+				{
+					va_end(args);
+					return (-1);
+				}
+				count += 2;
+			}
+		}
+		else
+		{
+			if (write(1, &format[i], 1) != 1)
+			{
+				va_end(args);
+				return (-1);
+			}
+			count++;
+		}
+		i++;
+	}
 
-    va_end(args);
-    return (count);
+	va_end(args);
+	return (count);
 }
